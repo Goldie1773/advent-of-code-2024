f9d1831ca29fc7fbb52170d9959968722be4714c
Goldie1773 <shadowhawk.gg@gmail.com>
2025-09-11 23:58:15 +0100

    Solve day-03 part one

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,1 +1,15 @@
-//
+use aoc_common::read_file_manifest;
+use regex::Regex;
+
+fn main() {
+    let input_text = read_file_manifest!("input.txt");
+
+    let re = Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
+    let mut mult_sum = 0;
+
+    for (_, [d_one, d_two]) in re.captures_iter(&input_text).map(|m| m.extract()) {
+        mult_sum += d_one.parse::<i32>().unwrap() * d_two.parse::<i32>().unwrap();
+    }
+
+    println!("Total sum of multiplications is: {mult_sum}");
+}
