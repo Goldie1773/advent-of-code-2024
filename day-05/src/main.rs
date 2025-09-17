4aa5de1732c056104e0cf28e289ae477877bb80f
Goldie1773 <shadowhawk.gg@gmail.com>
2025-09-17 19:02:03 +0100

    Setup day-05

---
+++ day-05/src/main.rs
@@ -0,0 +1,9 @@
+use aoc_common::read_file_manifest;
+
+fn main() {
+    let input = read_file_manifest!("input.txt");
+
+    // TODO: Implement day 5 solution here
+
+    println!("Day 5 solution not yet implemented");
+}
