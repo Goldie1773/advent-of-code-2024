f1bbcd522ac64e9b185e9373f1f9784528e09f0a
Goldie1773 <shadowhawk.gg@gmail.com>
2025-09-17 19:49:03 +0100

    Setup input parsing

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -1,9 +1,20 @@
 use aoc_common::read_file_manifest;
 
+fn split_input(input: String) -> (String, String) {
+    let mut parts = input.split("\r\n\r\n");
+    let page_order = parts.next().unwrap_or("").to_string();
+    let updates = parts.next().unwrap_or("").to_string();
+    (page_order, updates)
+}
+
 fn main() {
-    let input = read_file_manifest!("input.txt");
+    let input = read_file_manifest!("sample.txt");
+    let (page_order, updates) = split_input(input);
 
-    // TODO: Implement day 5 solution here
+    // Implement hash map for storing the relationship between the ordering pairs
+    // Use a stack to add the inputs and check each key against the value in the map in order
+    // Once verified correct capture the middle index
+    // Discard updates not in the correct order
 
-    println!("Day 5 solution not yet implemented");
+    println!("{page_order}, \n{updates}");
 }
