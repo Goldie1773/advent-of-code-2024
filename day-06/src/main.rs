0a810246320fa6e0da9eb47f555590feccfcd9d5
Goldie1773 <shadowhawk.gg@gmail.com>
2025-09-23 00:01:56 +0100

    Solve and optimise day-06 part 2 using rayon for parallelisation

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,8 +1,9 @@
 use std::collections::HashSet;
+use rayon::prelude::*;
 
 use aoc_common::read_file_manifest;
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 enum Direction {
     Right,
     Down,
@@ -46,22 +47,12 @@ fn parse_grid_and_find_guard(input: &str, guard: char) -> (Vec<Vec<char>>, (isiz
     (grid, guard_pos.expect("Grid will always contain a guard."))
 }
 
+
 fn is_in_bounds(pos: (isize, isize), rows: isize, cols: isize) -> bool {
     pos.0 >= 0 && pos.1 >= 0 && pos.0 < rows && pos.1 < cols
 }
 
-fn main() {
-    let (grid, mut guard_pos) = parse_grid_and_find_guard(&read_file_manifest!("input.txt"), '^');
-    let mut dir = Direction::Up;
-    let mut visited: HashSet<(isize, isize)> = HashSet::new();
-
-    // Logic:
-    // If the guard is blocked, rotate 90 degrees right, otherwise the guard moves forward
-    // Repeats until the guard has left the area
-
-    let grid_rows = grid.len() as isize;
-    let grid_cols = grid[0].len() as isize;
-
+fn original_path(grid: &Vec<Vec<char>>, mut guard_pos: (isize, isize), mut dir: Direction, visited: &mut HashSet<(isize, isize)>, grid_rows: isize, grid_cols: isize) {
     loop {
         visited.insert(guard_pos);
         let (dr, dc) = dir.offset();
@@ -80,9 +71,63 @@ fn main() {
             _ => unreachable!("Grid should only contain '.' or '#'."),
         }
     }
-
-    println!(
-        "The number of distinct positions the guard visited is {}",
-        visited.len()
-    );
 }
+
+fn check_loop(grid: &Vec<Vec<char>>, mut guard_pos: (isize, isize), mut dir: Direction, grid_rows: isize, grid_cols: isize) -> bool {
+    let mut visited_states: HashSet<((isize, isize), Direction)> = HashSet::new();
+    loop {
+        let state = (guard_pos, dir);
+        if visited_states.contains(&state) {
+            return true;  // Same position AND direction = loop!
+        }
+    
+        visited_states.insert(state);
+        let (dr, dc) = dir.offset();
+        let next_pos = (guard_pos.0 + dr, guard_pos.1 + dc);
+
+        if !is_in_bounds(next_pos, grid_rows, grid_cols) {
+            return false;
+        }
+
+        let cell = grid[next_pos.0 as usize][next_pos.1 as usize];
+
+        match cell {
+            '.' | '^' => guard_pos = next_pos,
+            '#' => dir = dir.rotate_right(),
+            _ => unreachable!("Grid should only contain '.' or '#'."),
+        }
+    }
+}
+
+fn main() {
+    let (grid, guard_pos) = parse_grid_and_find_guard(&read_file_manifest!("input.txt"), '^');
+    let dir = Direction::Up;
+    let mut visited: HashSet<(isize, isize)> = HashSet::new();
+
+    // Logic:
+    // If the guard is blocked, rotate 90 degrees right, otherwise the guard moves forward
+    // Repeats until the guard has left the area
+
+    let grid_rows = grid.len() as isize;
+    let grid_cols = grid[0].len() as isize;
+
+
+    original_path(&grid, guard_pos, dir, &mut visited, grid_rows, grid_cols);
+
+    // Optimised brute force search using rayon for parellisation
+    let num_obstacle_pos = visited.par_iter().filter(|(x, y)| {
+        let x = *x as usize;
+        let y = *y as usize;
+        let mut grid_clone = grid.clone();
+        let temp_symbol = grid_clone[x][y];
+        grid_clone[x][y] = '#';
+
+        let result = check_loop(&grid_clone, guard_pos, dir, grid_rows, grid_cols);
+
+        grid_clone[x][y] = temp_symbol;
+        result
+    }).count();
+
+    println!("The nubmber of distinct locations for an obstacle is {num_obstacle_pos}");
+}
+
