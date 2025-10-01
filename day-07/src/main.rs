b176073ead5cf4c80fb650647570bc9d30051cec
Goldie1773 <shadowhawk.gg@gmail.com>
2025-10-01 21:21:57 +0100

    Solve day-07 part 2

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -1,26 +1,40 @@
 use aoc_common::read_file_manifest;
+use rayon::str::ParallelString;
+use rayon::iter::ParallelIterator;
 
-const OPS: [fn(u64, u64) -> u64; 2] = [|a, b| a + b, |a, b| a * b];
+const OPS: [fn(u64, u64) -> u64; 3] = [
+    |a, b| a + b,
+    |a, b| a * b,
+    |a, b| concat(a, b)
+  ];
 
 fn target_in_permutations(nums: &[u64], target: u64) -> bool {
     let n = nums.len();
     if n == 0 {
         return false;
     }
-    (0..(1 << (n - 1))).any(|mask| {
+    let num_gaps = n - 1;
+    let total = 3u64.pow(num_gaps as u32);
+    (0..total).any(|mask| {
         let mut acc = nums[0];
-        for i in 0..n - 1 {
-            let op = (mask >> i) & 1;
+        let mut m = mask;
+        for i in 0..num_gaps {
+            let op = (m % 3) as usize;
             acc = OPS[op](acc, nums[i + 1]);
+            m /= 3;
         }
         acc == target
     })
 }
 
+fn concat(a: u64, b: u64) -> u64 {
+    a as u64 * 10u64.pow(b.ilog10() + 1) + b as u64
+}
+
 fn main() {
     let input = read_file_manifest!("input.txt");
     let total_cal_sum: u64 = input
-        .lines()
+        .par_lines()
         .filter_map(|line| {
             let mut parts = line.split(':');
             let target = parts.next()?.trim().parse::<u64>().ok()?;
