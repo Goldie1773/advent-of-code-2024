3c36f2aa8e2bdd4af2907278074efad454bad2f0
Goldie1773 <shadowhawk.gg@gmail.com>
2025-10-01 20:45:09 +0100

    Solve day-07 unoptimised

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -1,6 +1,46 @@
 use aoc_common::read_file_manifest;
+use regex::Regex;
+
+fn target_in_permutations(nums: &[u64], target: u64) -> bool {
+    let n = nums.len();
+    if n == 0 {
+        return false;
+    }
+    let ops = vec![|a, b| a + b, |a, b| a * b];
+
+    // There are 2^(n-1) operator combinations
+    for mask in 0..(1 << (n - 1)) {
+        let mut acc = nums[0];
+        for i in 0..n - 1 {
+            let op = (mask >> i) & 1;
+            acc = ops[op](acc, nums[i + 1]);
+        }
+        if acc == target {
+          return true;
+        } else {
+          continue;
+        }
+    }
+    return false
+}
 
 fn main() {
     let input = read_file_manifest!("input.txt");
-    println!("Day 7 solution not yet implemented");
+    
+    let re = Regex::new(r"(?<target>[0-9]+): (?<num_stream>.+)").unwrap();
+    let mut total_cal_sum = 0;
+
+    for line in input.lines() {
+      let caps = re.captures(line).unwrap();
+      let target: &u64= &caps["target"].parse::<u64>().unwrap();
+      let nums: Vec<u64> = caps["num_stream"].split(" ").map(|x| x.parse::<u64>().unwrap()).collect();
+
+      if target_in_permutations(&nums, *target) {
+        total_cal_sum += target
+      } else {
+        continue;
+      }
+
+    }
+    println!("Total calibration result is {total_cal_sum}");
 }
